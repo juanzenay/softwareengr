46f6328548bdf6f70f7c9d66d6842af4679f044f
juanzenay <65358942+juanzenay@users.noreply.github.com>
2022-11-02 19:47:45 -0500

    boxes implemented on text

--- src/pages/datetime/Datetime.jsx
+++ src/pages/datetime/Datetime.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
 import './datetime.css';
 
+import Box from '@mui/material/Box';
+
+/*
+for input - <input type= "varname" placeholder "placeholder"
+for button- button type = "button" > xxx <button />
+*/
+
 const Datetime = () => {
   return (
-    <div>Datetime</div>
+    <div>Datetime
+      <div className="gradient__bg">
+        <div className="datetime__box">
+          <Box sx={{ borderRadius: '10px' }}
+          height= {46} width = {200} fontSize= {24} 
+          display="flex" justifyContent="center" 
+          alignItems="center" bgcolor='black' color = 'white'>
+            Pick your date:
+            
+          </Box>
+        </div>
+        <div>
+          <Box sx={{ borderRadius: '10px' }}
+          height= {46} width = {200} fontSize= {24} 
+          display="flex" justifyContent="center" 
+          alignItems="center" bgcolor='black' color = 'white'>
+            Pick your time:
+            
+          </Box>
+        </div>
+        <div>
+          <Box sx={{ borderRadius: '10px' }}
+          height= {46} width = {962} fontSize= {24} 
+          m = 'auto' display="flex" justifyContent="center" 
+          alignItems="center" bgcolor='black' color = 'white'>
+            Credit card information and fee may be necessary to finalize reservation.
+            
+          </Box>
+        </div>
+      </div>
+    </div>
   )
 }
 
