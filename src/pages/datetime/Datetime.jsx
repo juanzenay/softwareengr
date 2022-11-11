455204be9ca3f863f67565794ec6eab1d8dc1dbb
juanzenay <65358942+juanzenay@users.noreply.github.com>
2022-11-11 15:54:51 -0600

    removed some random code

--- src/pages/datetime/Datetime.jsx
+++ src/pages/datetime/Datetime.jsx
@@ -4,54 +4,6 @@ import react, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-
-
-/*
-for input - <input type= "varname" placeholder "placeholder"
-for button- button type = "button" > xxx <button />
-
-for date picker- gray out past dates??
-for time picker?? preset times
-
-to finalize options implement button that says next and takes you to next page
-
-
-the screen is outputted on main screen temporarily for eaasier implementation
-
-//TODO-
-clean up code
-move as much as possible to app.css/index.css idk where it goes
-choose color scheme
-
-
-import Datepick from './Datepick';
-
-<div className="gradient__bg">
-        <div className="padded__small">
-          <div className='padded__small__text'>
-              Pick your date:
-          </div>
-        </div>
-        <div className='date__picker'>
-        <Datepick />
-        </div>
-        <div className = "padded__small__time">
-          <div className='padded__small__text'>
-            Pick your time:
-          </div>
-        </div>
-
-        <div className='long__bar'>
-          <div className='padded__bar__text'>
-            Credit card information and fee may be necessary to finalize reservation.
-          </div>
-        </div>
-      </div>
-
-export default Datetime
-
-*/
-
 const Datetime = () => {
   const [startDate, setStartDate] = useState(new Date());
 
