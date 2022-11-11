b8e2f567c857676473aff21161321b8a0e0fcbb3
juanzenay <65358942+juanzenay@users.noreply.github.com>
2022-11-11 15:44:59 -0600

    completeish

--- src/pages/datetime/Datetime.jsx
+++ src/pages/datetime/Datetime.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import './datetime.css';
-
+import react, { useState } from "react";
+import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-import Box from '@mui/material/Box';
+
 
 /*
 for input - <input type= "varname" placeholder "placeholder"
@@ -22,13 +23,10 @@ clean up code
 move as much as possible to app.css/index.css idk where it goes
 choose color scheme
 
-*/
+
 import Datepick from './Datepick';
 
-const Datetime = () => {
-  return (
-    <div>
-      <div className="gradient__bg">
+<div className="gradient__bg">
         <div className="padded__small">
           <div className='padded__small__text'>
               Pick your date:
@@ -47,6 +45,35 @@ const Datetime = () => {
           <div className='padded__bar__text'>
             Credit card information and fee may be necessary to finalize reservation.
           </div>
+        </div>
+      </div>
+
+export default Datetime
+
+*/
+
+const Datetime = () => {
+  const [startDate, setStartDate] = useState(new Date());
+
+  let handleColor = (time) => {
+    return time.getHours() > 12 ? "text-success" : "text-error";
+  };
+  return (
+    <div className = 'bg'>
+      <div className = 'main_rectangle'>
+        <p className='pick_date'>Pick the date and time for your reservation:</p>
+        <DatePicker 
+        className='date_container'
+        showTimeSelect
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        timeClassName={handleColor}
+        isClearable
+        placeholderText="Click Me"
+        /> 
+        <p className='card_notice'>Reservations on busy days may require a card on file and a no show fee</p>
+        <div className='confirm_button'>
+          <p className='confirm_text'>Confirm and continue</p>
         </div>
       </div>
     </div>
