bb7fe3d819c3e8feaf01c20424cc53af263f4683
EricIHernandez <36204758+EricIHernandez@users.noreply.github.com>
2022-11-17 19:11:24 -0600

    Added Routes to LoginPage and GuestInfoPage

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import { Navbar } from './components'; //add in any components
 import { Footer, Header } from './containers'; //adds in containers
 //import { Datetime } from './pages'; //add in any pages
 import Checkout from './pages/checkout/checkout.jsx';
-import Datetime from './pages/datetime/Datetime.jsx';
+import ViewReservation from './pages/viewReservation/viewreservation.jsx';
+import LoginPage from './pages/loginPage/loginPage.jsx';
+import GuestInfo from './pages/guestInformation/guestInformation.jsx';
 
 import ViewReservation from './pages/viewReservation/viewreservation.jsx';
 import { Route, Routes, Navigate, useNavigate } from 'react-router-dom';
@@ -18,8 +20,10 @@ const App = () => {
     <div classname = "App">
       <Routes>
         <Route path="/datetime" element={<Datetime navigate = {navigate}/>} />
-        <Route path="/checkout" element={<Checkout navigate = {navigate}/>} />
-        <Route path="/reservation" element={<ViewReservation navigate = {navigate}/>} />
+					<Route path="/checkout" element={<Checkout navigate = {navigate}/>} />
+					<Route path="/reservation" element={<ViewReservation navigate = {navigate}/>} />
+          <Route path="/login" element={<LoginPage navigate = {navigate}/>} />
+          <Route path="/guest" element={<GuestInfo navigate = {navigate}/>} />
         <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
     </div>
