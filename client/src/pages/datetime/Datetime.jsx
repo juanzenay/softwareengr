5566631c741c1e4594eb83d39c22d18e4611870a
juanzenay <65358942+juanzenay@users.noreply.github.com>
2022-12-01 14:31:53 -0600

    Update Datetime.jsx

--- client/src/pages/datetime/Datetime.jsx
+++ client/src/pages/datetime/Datetime.jsx
@@ -6,19 +6,18 @@ import "react-datepicker/dist/react-datepicker.css";
 
 const Datetime = ({navigate}) => {
 
-  const[reservation_Date, setReservationDate] = useState('');
-  const[reservation_Time, setReservationTime] = useState('');
-
   const handlesubmit = event => {
-    console.log('handlesubmit ran');
     event.preventDefault();
-    console.log('Date: ', reservation_Date);
-    console.log('Time: ', reservation_Time);
+    console.log(startDate);
+
+    navigate('/checkout')
+    //console.log('Date: ', reservation_Date);
+    //console.log('Time: ', reservation_Time);
   }
 
-  const setData = (date, time) => {
+  const setData = event => {
     
-    //navigate('/reservation')
+    navigate('/reservation')
   }
   const [startDate, setStartDate] = useState(new Date());
 
@@ -29,19 +28,19 @@ const Datetime = ({navigate}) => {
     <div className = 'bg'>
       <div className = 'main_rectangle'>
         <form onSubmit={handlesubmit}>
-          <label className='pick_date'>Type the date and time for your reservation and press enter:</label>
-          <input
-          type = "text"
-          id = "reservationDate"
-          placeholder="MM/DD/YY"
-          required = "true"
-          className= "date_container"
-          value={reservation_Date}
-          onChange={event => setReservationDate(event.target.value)}
+          <label className='pick_date'>Pick the date and time for your reservation:</label>
+          <DatePicker 
+            className='date_container'
+            showTimeSelect
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+            timeClassName={handleColor}
+            isClearable
+            placeholderText="Click Me"
           /> 
         </form>
         <label className='card_notice'>Reservations on busy days may require a card on file and a no show fee</label>
-        <button type= "submit" className='confirm_button'> 
+        <button type= "submit" className='confirm_button' onClick = {handlesubmit}> 
           <label className='confirm_text'>Confirm and continue</label>
         </button> 
       </div>
@@ -52,13 +51,13 @@ const Datetime = ({navigate}) => {
 export default Datetime
 
 /*
-<DatePicker 
-        className='date_container'
-        showTimeSelect
-        selected={startDate}
-        onChange={(date) => setStartDate(date)}
-        timeClassName={handleColor}
-        isClearable
-        placeholderText="Click Me"
-        /> 
+  <input
+          type = "text"
+          id = "reservationDate"
+          placeholder="MM/DD/YY"
+          required = "true"
+          className= "date_container"
+          value={reservation_Date}
+          onChange={event => setReservationDate(event.target.value)}
+          />
 */
